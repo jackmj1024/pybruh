b21ca84a6d2272ddc1bb7a942da7b93d893bb62c
oshaughnessya <70730300+oshaughnessya@users.noreply.github.com>
2023-05-16 01:02:43 -0500

    converted the random interval to use the sigmoid function

--- pybruh.py
+++ pybruh.py
@@ -1,12 +1,12 @@
 import os
 import time
 import random
+import math
 from pydub import AudioSegment
 from pydub.playback import play
 
 
 SOUND_DIRECTORY = os.path.abspath('sounds')  # Directory to load sounds from.
-MIN_INTERVAL = 5*60  # Minimum time between sounds in seconds.
 MAX_INTERVAL = 60*60  # Maximum time between sounds in seconds.
 
 
@@ -32,14 +32,21 @@ def play_random_sound(options, do_logging=False):
         print('Played sound: ' + file)
 
 
-def do_random_loop(min_seconds, max_seconds, sounds, do_logging=False):
+def do_random_loop(max_seconds, sounds, do_logging=False):
     while True:
-        delay = random.randint(min_seconds, max_seconds)
+        delay = calculate_delay_time(max_seconds)
         if do_logging:
             print("Delay set to " + str(delay) + " seconds.")
         time.sleep(delay)
         play_random_sound(sounds, do_logging)
 
+def calculate_delay_time(max_seconds):
+    x = random.uniform(0, 10)
+    x = 6 - x
+    x = math.e**x
+    x = 1 + x
+    x = max_seconds / x
+    return int(x)
 
 library = get_valid_files(True)
-do_random_loop(MIN_INTERVAL, MAX_INTERVAL, library, True)
+do_random_loop(MAX_INTERVAL, library, True)
